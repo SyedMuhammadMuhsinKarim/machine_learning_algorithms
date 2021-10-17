544ba6d03e09b036bea23faee3dc3b8046ec79eb
Syed Muhammad Muhsin Karim <37833481+SyedMuhammadMuhsinKarim@users.noreply.github.com>
2021-10-18 01:11:18 +0500

    Update unsupervised_learning_clustering_affinity_propagation.py

--- unsupervised_learning_clustering_affinity_propagation.py
+++ unsupervised_learning_clustering_affinity_propagation.py
@@ -10,47 +10,47 @@ class AffinityPropagation:
 
     def __find_similarity_matrix(self): 
         sim_matrix = np.zeros((self.org_matrix_len, self.org_matrix_len), dtype=int)
-        
-        for i in range(len(self.org_matrix)):
-            for j in range(len(self.org_matrix)):
-                sim_matrix[i, j] = - sum((self.org_matrix[i] - self.org_matrix[j]) ** 2)
 
-        for i in range(len(self.org_matrix)):
-            for j in range(len(self.org_matrix)):
-                if i == j:
-                    sim_matrix[i, j] = sim_matrix.min() 
+        for idx, _ in np.ndenumerate(self.org_matrix):
+            ith_row = self.org_matrix[idx[0]]
+            jth_row = self.org_matrix[idx[1]]
+            power_of_diff = (ith_row - jth_row) ** 2
+            sim_matrix[idx[0], idx[1]] = - sum(power_of_diff)
+
+        for idx, _ in np.ndenumerate(sim_matrix):
+            if idx[0] == idx[1]: sim_matrix[idx[0], idx[1]] = sim_matrix.min() 
 
         return self.__find_responsibility_matrix(sim_matrix)
 
     def __find_responsibility_matrix(self, sim_matrix):
         responsibility_matrix = np.zeros((self.org_matrix_len, self.org_matrix_len), dtype=int)    
 
-        for i in range(self.org_matrix_len):
-            for j in range(self.org_matrix_len):
-                row_excludng = (sim_matrix[i, :j], sim_matrix[i, j+1:])
-                row_excludng_max = max(np.concatenate(row_excludng))
-                responsibility_matrix[i, j] = sim_matrix[i, j] - row_excludng_max
+        for idx, _ in np.ndenumerate(sim_matrix):
+            i, j = idx
+            row_excludng = (sim_matrix[i, :j], sim_matrix[i, j+1:])
+            row_excludng_max = max(np.concatenate(row_excludng))
+            responsibility_matrix[i, j] = sim_matrix[i, j] - row_excludng_max
                 
         return self.__find_availibilty_matrix(responsibility_matrix)
 
     def __find_availibilty_matrix(self, responsibility_matrix):
         avail_matrix = np.zeros((self.org_matrix_len, self.org_matrix_len), dtype=int)   
 
-        for i in range(self.org_matrix_len):
-            for j in range(self.org_matrix_len):
-                if i == j: 
-                    col_exclud = (responsibility_matrix[:i, j], responsibility_matrix[i:, j])
-                    col_exclud = np.concatenate(col_exclud)
-                    avail_matrix[i, j] = np.int64((col_exclud +  np.abs(col_exclud)).sum()/2)
-                else:
-                    exclude = (responsibility_matrix[:i, j], responsibility_matrix[i:, j])
-                    exclude = np.concatenate(exclude)
-                    if len(exclude) > 1:
-                        subs = np.zeros((len(exclude)), dtype=int)
-                        subs[i], subs[j] = responsibility_matrix[i,j], responsibility_matrix[j,j]
-                    col_exclud = exclude - subs
-                    inclusion =  responsibility_matrix[j,j] + np.int64((col_exclud +  np.abs(col_exclud)).sum()/2)
-                    avail_matrix[i,j] = min([0, inclusion]) 
+        for idx, _ in np.ndenumerate(self.org_matrix):
+            i, j = idx
+            if i == j: 
+                col_exclud = (responsibility_matrix[:i, j], responsibility_matrix[i:, j])
+                col_exclud = np.concatenate(col_exclud)
+                avail_matrix[i, j] = np.int64((col_exclud +  np.abs(col_exclud)).sum()/2)
+            else:
+                exclude = (responsibility_matrix[:i, j], responsibility_matrix[i:, j])
+                exclude = np.concatenate(exclude)
+                if len(exclude) > 1:
+                    subs = np.zeros((len(exclude)), dtype=int)
+                    subs[i], subs[j] = responsibility_matrix[i,j], responsibility_matrix[j,j]
+                col_exclud = exclude - subs
+                inclusion =  responsibility_matrix[j,j] + np.int64((col_exclud +  np.abs(col_exclud)).sum()/2)
+                avail_matrix[i,j] = min([0, inclusion]) 
                 
         return self.__find_criterion_matrix(responsibility_matrix, avail_matrix)
 
